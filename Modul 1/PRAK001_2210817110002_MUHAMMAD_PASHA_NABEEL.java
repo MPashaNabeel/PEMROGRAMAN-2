c987e85d5b317919530d980031cc732c1214bd98
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2023-09-19 02:42:01 +0800

    Delete Modul 1 directory

--- Modul 1/PRAK001_2210817110002_MUHAMMAD_PASHA_NABEEL.java
+++
@@ -1,8 +0,0 @@
-public class PRAK001_2210817110002_MUHAMMAD_PASHA_NABEEL {
-	
-	 public static void main(String[] args) {
-		 
-		 System.out.println("Selamat Pagi, Muhammad Pasha Nabeel \nSelamat Siang, Muhammad Pasha Nabeel \nSelamat Malam, Muhammad Pasha Nabeel");
-	       
-	 }
-}
