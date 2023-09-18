0b11c3b53c87e6eaf9ec753129353479e3029e09
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2023-09-19 02:43:49 +0800

    Add files via upload

---
+++ Modul 1/soal 4/PRAK004_2210817110002_MUHAMMAD_PASHA_NABEEL.java
@@ -0,0 +1,7 @@
+public class PRAK004_2210817110002_MUHAMMAD_PASHA_NABEEL {
+   public static void main(String[] args) {
+        String nama = "Muhammad Pasha Nabeel";
+        int umur = 19 ;
+         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
+          }  
+}
