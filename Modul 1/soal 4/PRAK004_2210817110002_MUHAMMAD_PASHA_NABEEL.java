d95dac1f6902a3ea9640cd81131c2197e23ec9f4
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2023-10-03 16:57:25 +0800

    Delete Modul 1 directory

--- Modul 1/soal 4/PRAK004_2210817110002_MUHAMMAD_PASHA_NABEEL.java
+++
@@ -1,7 +0,0 @@
-public class PRAK004_2210817110002_MUHAMMAD_PASHA_NABEEL {
-   public static void main(String[] args) {
-        String nama = "Muhammad Pasha Nabeel";
-        int umur = 19 ;
-         System.out.println("Perkenalkan, Nama Saya "+ nama +", Umur Saya " + umur);
-          }  
-}
