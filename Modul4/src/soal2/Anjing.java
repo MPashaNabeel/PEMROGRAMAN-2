e31cf052abbeb99012ee74a7fc450d1877141c21
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2023-11-16 22:00:09 +0800

    Pemrograman 2

---
+++ Modul4/src/soal2/Anjing.java
@@ -0,0 +1,17 @@
+package soal2;
+
+public class Anjing extends Peliharaan {
+    private final String warnaBulu;
+    private final String kemampuan;
+
+    public Anjing(String r, String n, String warnaBulu, String kemampuan) {
+        super(r, n);
+        this.warnaBulu = warnaBulu;
+        this.kemampuan = kemampuan;
+    }
+    public void displayDetailAnjing() {
+        super.display();
+        System.out.println("Warna Bulu: " + warnaBulu);
+        System.out.println("Kemampuan: " + kemampuan);
+    }
+}
